01c9172a5e0c6abd7c527322ad0a18bb98a19b30
Cae12345 <caetanocapanema@hotmail.com>
2023-05-03 23:47:27 -0300

    EP2 - Define Posições

--- ep2.py
+++ ep2.py
@@ -1,1 +1,8 @@
-print('carro1')
+def define_posicoes(linha,col,ori,tam):
+    posi = []
+    for i in range(tam):
+        if ori == 'vertical':
+            posi.append([linha+i,col])
+        if ori == 'horizontal':
+            posi.append([linha,col+i])
+    return posi
